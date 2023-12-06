6123b1e6f4b57273517056f8513b32543e2077fc
Stefan Katsarov <59067555+StefKats@users.noreply.github.com>
2023-12-06 23:32:21 +0100

    Update generate_energy_reco.sh

--- generate_energy_reco.sh
+++ generate_energy_reco.sh
@@ -5,15 +5,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo $SCRIPT_DIR
 
-for dir in z_*/; do rsync -v -aP ~/PGsim2/z_291/. $dir;done # copy the first energy dir
+for dir in z_*/; do rsync -v -aP ~/SND-LHC/z_291/. $dir;done # copy the first energy dir
 
-for dir in z_*/; do cp -v ~/PGsim2/z_291/generate_PDF.sh $dir;done # copy file to mulitple folders
+for dir in z_*/; do cp -v ~/SND-LHC/z_291/generate_PDF.sh $dir;done # copy file to mulitple folders
 
-for dir in z_*/; do cp -v ~/PGsim2/z_291/energy_reco.sh $dir;done # copy file to mulitple folders
+for dir in z_*/; do cp -v ~/SND-LHC/z_291/energy_reco.sh $dir;done # copy file to mulitple folders
 
-for dir in z_*/; do cp -v ~/PGsim2/z_291/energy_reco.sub $dir;done # copy file to mulitple folders
+for dir in z_*/; do cp -v ~/SND-LHC/z_291/energy_reco.sub $dir;done # copy file to mulitple folders
 
-for dir in z_*/; do cp -v ~/PGsim2/z_291/update_energy_reco.sh $dir;done # copy file to mulitple folders
+for dir in z_*/; do cp -v ~/SND-LHC/z_291/update_energy_reco.sh $dir;done # copy file to mulitple folders
 
 for dir in z_*/; do bash "$dir"update_energy_reco.sh;done # run update script for all files
 
